4bd0b526d8628662f2a4384edcc8ba1a81b190d6
tanmay17268 <tanmay17268@iiitd.ac.in>
2018-12-01 14:25:22 +0530

    snake crossing walls from right to left, bug resolved

--- src/Sprite.java
+++ src/Sprite.java
@@ -106,7 +106,7 @@ public abstract class Sprite {
 	}
 
 	public void forceSpriteOnRight(Sprite s) {
-		s.setPositionX(positionX + width+1);
+		s.setPositionX(positionX + width+2);
 	}
 
 	public void forceSpriteOnBottom(Sprite s) {
